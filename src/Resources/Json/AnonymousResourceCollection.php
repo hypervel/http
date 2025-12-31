8a328c90e75c8f1247ebc01a17cb55e53027b47a
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2025-12-31 03:22:50 +0000

    Add UseResource and UseResourceCollection attributes
    Port Laravel's resource attributes for declarative model-to-resource binding:
    
    - Add #[UseResource] attribute for binding JsonResource to models
    - Add #[UseResourceCollection] attribute for binding ResourceCollection to models
    - Add TransformsToResource trait providing toResource() on models
    - Add TransformsToResourceCollection trait providing toResourceCollection() on collections
    - Fix AnonymousResourceCollection to properly extend Hypervel's ResourceCollection

--- src/Resources/Json/AnonymousResourceCollection.php
+++ src/Resources/Json/AnonymousResourceCollection.php
@@ -4,8 +4,24 @@ declare(strict_types=1);
 
 namespace Hypervel\Http\Resources\Json;
 
-use Hyperf\Resource\Json\AnonymousResourceCollection as BaseAnonymousResourceCollection;
-
-class AnonymousResourceCollection extends BaseAnonymousResourceCollection
+/**
+ * Anonymous resource collection for wrapping arbitrary collections.
+ *
+ * This class extends ResourceCollection to ensure proper type hierarchy
+ * within Hypervel's resource system.
+ */
+class AnonymousResourceCollection extends ResourceCollection
 {
+    /**
+     * Create a new anonymous resource collection.
+     *
+     * @param mixed $resource the resource being collected
+     * @param string $collects the name of the resource being collected
+     */
+    public function __construct(mixed $resource, string $collects)
+    {
+        $this->collects = $collects;
+
+        parent::__construct($resource);
+    }
 }
